0e8aad3a26b494fcc05cb60cb1d82806ccfd9551
Jordan Sexton <jordan@jordansexton.com>
2014-04-04 23:26:09 -0500

    Fixing incorrect module name

--- lib/police/gem_version.rb
+++ lib/police/gem_version.rb
@@ -1,6 +1,6 @@
 require 'rubygems/version'
 
-module RailsUtils
+module Police
   # Returns the version of the currently loaded Police as a <tt>Gem::Version</tt>
   def self.gem_version
     Gem::Version.new VERSION::STRING
