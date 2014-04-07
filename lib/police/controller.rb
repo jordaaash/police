42159e965129ae5b8edc67694990e0f4f92a121e
Jordan Sexton <jordan@jordansexton.com>
2014-04-07 15:18:18 -0500

    Refactoring police_user to remove class attributes

--- lib/police/controller.rb
+++ lib/police/controller.rb
@@ -7,19 +7,27 @@ require 'action_controller/base'
 module Police
   module Controller
     def police (options = {}, &block)
-      options     = {
-        :user_method => :current_user,
-        :user_class  => 'User'
+      options    = {
+        :user       => :current_user,
+        :user_class => 'User'
       }.merge!(options)
-      user_method = options.delete :user_method
-      user_class  = options.delete :user_class
+      user       = options.delete :user
+      user_class = options.delete :user_class
+
+      police_user = []
+      police_user << "#{user}" if user
+      police_user << "#{user_class}.new" if user_class
+      police_user << 'nil' if police_user.empty?
+      police_user = police_user.compact.join(' || ')
 
       include Police
       include InstanceMethods
 
-      class_attribute :police_user_method, :police_user_class
-      self.police_user_method = user_method
-      self.police_user_class  = user_class
+      class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+        def police_user                   # def police_user
+          @police_user ||= #{police_user} #   @police_user ||= current_user || User.new
+        end                               # end
+      RUBY_EVAL
 
       hide_action :model_base_classes, :policies, :policy_class, :model_class
       helper_method :police!, :police?, :authorize!, :authorized?, :can?,
@@ -36,29 +44,6 @@ module Police
           user, action = police_user, user
         end
         super
-      end
-
-      def police_user
-        if @police_user
-          @police_user
-        else
-          unless (police_user = send police_user_method)
-            user_class = police_user_class
-            if user_class
-              user_class = user_class.constantize if user_class.is_a?(String)
-              police_user = user_class.new
-            end
-          end
-          @police_user = police_user
-        end
-      end
-
-      def police_user_method
-        self.class.police_user_method
-      end
-
-      def police_user_class
-        self.class.police_user_class
       end
     end
   end
